196ec5906c98c432303de5bfc01da085041e517b
KWystan <karlwystan.cabalonga@wvsu.edu.ph>
2026-07-11 19:41:51 +0800

    feat: add /project/:id and /chat/:conversationId routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ChatPage from './components/ChatPage';
 import GeneratePage from './components/GeneratePage';
+import ProjectPage from './components/ProjectPage';
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<ChatPage />} />
         <Route path="/chat" element={<ChatPage />} />
+        <Route path="/chat/:conversationId" element={<ChatPage />} />
+        <Route path="/project/:id" element={<ProjectPage />} />
         <Route path="/generate" element={<GeneratePage />} />
       </Routes>
     </BrowserRouter>
